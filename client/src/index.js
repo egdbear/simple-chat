2628d563f86745e358703fab47dbc476629a6b15
Dejan Tomic <dtomic@codevise.de>
2017-06-10 19:09:17 +0200

    Small improvements

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 
 import { createStore } from 'redux';
 
-const store = createStore(()=> null);
+const store = createStore(() => null);
 
 ReactDOM.render(
   <Provider store={store}>
