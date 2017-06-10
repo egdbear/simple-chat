3cd7e3d1e693e519c5601f1ec703e3ebf8f65548
Dejan Tomic <dtomic@codevise.de>
2017-06-10 16:40:09 +0200

    Reogranise files

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './assets/logo.svg';
 import './styles/App.css';
 
-class App extends Component.PureComponent {
+class App extends Component {
   render() {
     return (
       <div className="App">
