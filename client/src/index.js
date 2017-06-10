770dc357332e3828b16b17d222ba08357b07eb21
Dejan Tomic <dtomic@codevise.de>
2017-06-10 17:01:07 +0200

    Add redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/index.css';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+
+const store = createStore(()=> null);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
