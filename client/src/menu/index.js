19798806647fbb724b9ea49348f2740c3ddadba2
Dejan Tomic <dtomic@codevise.de>
2017-06-12 23:08:10 +0200

    Fix lint

--- client/src/menu/index.js
+++ client/src/menu/index.js
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom';
 
 export default (props) => {
   return (
-     <ul>
+    <ul>
       <li><Link to="/">Home</Link></li>
+      <li><Link to="/signup">Sign up</Link></li>
       <li><Link to="/about">About</Link></li>
     </ul>
   );
