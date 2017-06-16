b472f93cbc8e001f2fe19f1f7d8a6f2659e70622
Dejan Tomic <dtomic@codevise.de>
2017-06-16 21:07:50 +0200

    Send user data on login

--- server/routes/login-route.js
+++ server/routes/login-route.js
@@ -3,8 +3,5 @@ const router = express.Router();
 const loginService = require('../services/login-service');
 
 router.post('/login', loginService);
-router.get('/room/*', function(req, res) {
-  res.status(200).json({test: 'test'})
-});
 
 module.exports = router;
--- server/services/login-service.js
+++ server/services/login-service.js
@@ -28,7 +28,10 @@ module.exports = function (req, res, next) {
             const payload = { id: user._id };
             const token = jwt.encode(payload, config.JWTSecret);
 
+            const { name, _id, email } = user;
+
             res.json({
+                user: {name, _id, email},
                 token: token
             });
           });
