e1f1a2c03d0a61f22496323107e401abcf66ae72
Dejan Tomic <dtomic@codevise.de>
2017-06-16 20:11:55 +0200

    Add more socket things

--- server/models/Room.js
+++ server/models/Room.js
@@ -1,4 +1,5 @@
 const mongoose  = require('mongoose');
+const _ = require('lodash');
 
 const RoomSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -7,6 +8,16 @@ const RoomSchema = new mongoose.Schema({
 
 RoomSchema.methods.listRooms = function(callback) {
   return callback(null, [{id: 1, name: 'TestRoom'}, {id: 2, name: 'Tech'}, {id: 3, name: 'Lounge'}])
+}
+
+const messages =  [
+  {id: 1, messages:[{id: 1, message: 'test'}, {id: 2, message: 'test2'}], roomId: 1},
+  {id: 2, messages:[{id: 34, message: 'some awkward'}, {id: 35, message: 'ldldldl'}], roomId: 2},
+  {id: 3, messages:[{id: 43, message: 'owowoowow'}, {id: 44, message: 'owowowowowo'}], roomId: 3},
+];
+
+RoomSchema.methods.listMessages = function(roomId, callback) {
+  return callback(null, _.find(messages, {roomId: roomId}));
 }
 
 var roomModel = mongoose.model('room', RoomSchema);
--- server/sockets/index.js
+++ server/sockets/index.js
@@ -2,11 +2,20 @@ const Room = require('../models/Room');
 
 module.exports = function (io) {
   const room = new Room();
+
   io.on('connection', function (socket) {
     socket.on('list-rooms', function (data) {
       room.listRooms(function(err, rooms) {
-        socket.emit('updated-rooms-list', rooms);
+        socket.emit('list-rooms', rooms);
       })
+
     });
+
+    socket.on('message', message => {
+      socket.broadcast.emit('message', {
+        body: message.body,
+        from: socket.id.slice(8)
+      })
+    })
   });
 }
